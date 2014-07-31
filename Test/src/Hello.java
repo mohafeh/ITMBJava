648c4914133a9674de13dd845b614762393ddffc
cssgit <christian.sauer@uwl.ac.uk>
2014-07-31 14:54:12 +0100

    edited hello fehmida

--- Test/src/Hello.java
+++ Test/src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello Fehmida");
 		// TODO Auto-generated method stub
 
 	}
